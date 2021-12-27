22e75ae8c8792bfae4038945d85d5af12cf7272c
Gabriel Tan-Chen <gabriel@tan-chen.com>
2021-12-27 15:39:50 -0500

    Update migration files

--- migrations/20191126191946-create-subcategories-table.js
+++ migrations/20191126191946-create-subcategories-table.js
@@ -1,41 +1,41 @@
-'use strict';
-/* istanbul ignore next */
-module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('subcategories', {
-      uuid: {
-        allowNull: false,
-        primaryKey: true,
-        type: Sequelize.UUID,
-      },
-      created_at: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      updated_at: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      deleted_at: {
-        allowNull: true,
-        type: Sequelize.DATE,
-      },
-      category_uuid: {
-        allowNull: false,
-        references: {
-          key: 'uuid',
-          model: 'categories',
-        },
-        type: Sequelize.UUID,
-      },
-      name: {
-        allowNull: false,
-        type: Sequelize.STRING,
-      },
-    });
-  },
+const { Sequelize } = require('sequelize');
 
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('subcategories');
-  },
-};
+async function up({ context: queryInterface }) {
+  await queryInterface.createTable('subcategories', {
+    uuid: {
+      allowNull: false,
+      primaryKey: true,
+      type: Sequelize.UUID,
+    },
+    created_at: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    updated_at: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    deleted_at: {
+      allowNull: true,
+      type: Sequelize.DATE,
+    },
+    category_uuid: {
+      allowNull: false,
+      references: {
+        key: 'uuid',
+        model: 'categories',
+      },
+      type: Sequelize.UUID,
+    },
+    name: {
+      allowNull: false,
+      type: Sequelize.STRING,
+    },
+  });
+}
+
+async function down({ context: queryInterface }) {
+  await queryInterface.dropTable('subcategories');
+}
+
+module.exports = { up, down };
