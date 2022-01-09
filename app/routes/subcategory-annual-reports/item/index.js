3df7bb84e7d671c8116f311394e7780a8b0dbb46
Gabriel Tan-Chen <gabriel@tan-chen.com>
2022-01-09 11:40:39 -0500

    Implement route for individual subcategory annual reports

--- app/routes/subcategory-annual-reports/item/index.js
+++ app/routes/subcategory-annual-reports/item/index.js
@@ -1,16 +1,11 @@
 const getFn = require('./get');
-const routeItem = require('./item');
 
 module.exports = (router, app) => {
   const Authentication = app.get('Authentication');
 
-  router.route('/')
+  return router.route('/:id')
     .get(
       Authentication.UserAuth.can('access-account'),
       getFn(app),
     );
-
-  routeItem(router, app);
-
-  return router;
 };
