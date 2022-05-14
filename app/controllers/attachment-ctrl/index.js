07cd5185fa0f4f50f899adc4f49ee90e1d8fb815
Gabriel Tan-Chen <gabriel@tan-chen.com>
2022-05-14 19:42:13 -0400

    AttachmentCtrl.uploadAttachment

--- app/controllers/attachment-ctrl/index.js
+++ app/controllers/attachment-ctrl/index.js
@@ -1,11 +1,15 @@
+const { S3Client } = require('@aws-sdk/client-s3');
+
 const createAttachment = require('./create-attachment');
 const deleteAttachment = require('./delete-attachment');
 const updateAttachment = require('./update-attachment');
+const uploadAttachment = require('./upload-attachment');
 
 class AttachmentCtrl {
   constructor(parent, models) {
     this.parent = parent;
     this.models = models;
+    this.s3Client = new S3Client({ region: 'us-east-1' });
   }
 
   async createAttachment(params) {
@@ -24,6 +28,13 @@ class AttachmentCtrl {
 
   async updateAttachment(params) {
     return updateAttachment({
+      ...params,
+      attachmentCtrl: this,
+    });
+  }
+
+  async uploadAttachment(params) {
+    return uploadAttachment({
       ...params,
       attachmentCtrl: this,
     });
---
+++ app/controllers/attachment-ctrl/upload-attachment.js
@@ -0,0 +1,106 @@
+const { HeadObjectCommand, PutObjectCommand } = require('@aws-sdk/client-s3');
+const nconf = require('nconf');
+const Sequelize = require('sequelize');
+
+const { AttachmentError } = require('../../middleware/error-handler');
+
+/**
+ * @param {object} attachmentCtrl Instance of AttachmentCtrl
+ * @param {string} attachmentUuid
+ * @param {string} auditApiCallUuid
+ * @param {buffer} fileBody
+ * @param {string} fileName
+ */
+module.exports = async({
+  attachmentCtrl,
+  attachmentUuid,
+  auditApiCallUuid,
+  fileBody,
+  fileName,
+}) => {
+  const controllers = attachmentCtrl.parent;
+  const models = attachmentCtrl.models;
+  if (!attachmentUuid) {
+    throw new AttachmentError('Attachment is required');
+  } else if (!fileName) {
+    throw new AttachmentError('File name is required');
+  } else if (!fileBody) {
+    throw new AttachmentError('File body is required');
+  }
+
+  const apiCall = await models.Audit.ApiCall.findOne({
+    attributes: ['user_uuid', 'uuid'],
+    where: {
+      uuid: auditApiCallUuid,
+    },
+  });
+  if (!apiCall || !apiCall.get('user_uuid')) {
+    throw new AttachmentError('Missing audit API call');
+  }
+
+  const user = await models.User.findOne({
+    attributes: ['household_uuid', 'uuid'],
+    where: {
+      uuid: apiCall.get('user_uuid'),
+    },
+  });
+  if (!user) {
+    throw new AttachmentError('Audit user does not exist');
+  }
+
+  const attachment = await models.Attachment.findOne({
+    attributes: ['name', 'uuid'],
+    include: [{
+      as: 'Expense',
+      attributes: ['uuid'],
+      include: [{
+        attributes: ['uuid'],
+        include: [{
+          attributes: ['uuid'],
+          model: models.Category,
+          required: true,
+          where: {
+            household_uuid: user.get('household_uuid'),
+          },
+        }],
+        model: models.Subcategory,
+        required: true,
+      }],
+      model: models.Expense,
+      required: true,
+    }],
+    where: {
+      uuid: attachmentUuid,
+    },
+  });
+  if (!attachment) {
+    throw new AttachmentError('Not found');
+  }
+
+  await controllers.AttachmentCtrl.s3Client.send(new PutObjectCommand({
+    Bucket: nconf.get('AWS_STORAGE_BUCKET'),
+    Key: fileName,
+    Body: fileBody,
+  }));
+
+  const headResponse = await controllers.AttachmentCtrl.s3Client.send(new HeadObjectCommand({
+    Bucket: nconf.get('AWS_STORAGE_BUCKET'),
+    Key: fileName,
+  }));
+
+  attachment.set('aws_bucket', nconf.get('AWS_STORAGE_BUCKET'));
+  attachment.set('aws_key', fileName);
+  attachment.set('aws_etag', headResponse.ETag);
+  attachment.set('aws_content_length', headResponse.ContentLength);
+  attachment.set('aws_content_type', headResponse.ContentType);
+
+  await models.sequelize.transaction({
+    isolationLevel: Sequelize.Transaction.ISOLATION_LEVELS.REPEATABLE_READ,
+  }, async(transaction) => {
+    await controllers.AuditCtrl.trackChanges({
+      auditApiCallUuid,
+      changeList: [attachment],
+      transaction,
+    });
+  });
+};
