77f2e16600c955ddfcbda8a03e9c52d6ed711ebd
Gabriel Tan-Chen <gabriel@tan-chen.com>
2022-06-10 15:35:40 -0700

    PATCH /attachments/:uuid

--- app/routes/attachments/item/index.js
+++ app/routes/attachments/item/index.js
@@ -1,6 +1,23 @@
+const { body } = require('express-validator');
+const patchFn = require('./patch');
 const routeUpload = require('./upload');
 
 module.exports = (router, app) => {
+  const Auditor = app.get('Auditor');
+  const Authentication = app.get('Authentication');
+  const Validator = app.get('Validator');
+
+  router.route('/:uuid')
+    .patch(
+      Authentication.UserAuth.can('access-account'),
+      [
+        body([['data', 'attributes', 'name']], 'Attachment name is required.').not().isEmpty(),
+      ],
+      Validator.validateRequest(),
+      Auditor.trackApiCall(),
+      patchFn(app),
+    );
+
   routeUpload(router, app);
 
   return router;
