3614058756526030b865d4ec301a84e55f0af92a
Gabriel Tan-Chen <gabriel@tan-chen.com>
2022-07-21 23:57:49 -0400

    Create migration

---
+++ migrations/20220716184354-create-projected-incomes-table.js
@@ -0,0 +1,49 @@
+const { Sequelize } = require('sequelize');
+
+async function up({ context: queryInterface }) {
+  await queryInterface.createTable('projected_incomes', {
+    uuid: {
+      allowNull: false,
+      primaryKey: true,
+      type: Sequelize.UUID,
+    },
+    created_at: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    updated_at: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    deleted_at: {
+      allowNull: true,
+      type: Sequelize.DATE,
+    },
+    household_member_uuid: {
+      allowNull: false,
+      references: {
+        key: 'uuid',
+        model: 'household_members',
+      },
+      type: Sequelize.UUID,
+    },
+    date: {
+      allowNull: false,
+      type: Sequelize.DATEONLY,
+    },
+    amount_cents: {
+      allowNull: false,
+      type: Sequelize.INTEGER,
+    },
+    description: {
+      allowNull: false,
+      type: Sequelize.STRING,
+    },
+  });
+}
+
+async function down({ context: queryInterface }) {
+  await queryInterface.dropTable('projected_incomes');
+}
+
+module.exports = { up, down };
