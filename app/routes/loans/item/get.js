271dceba727302b513d87ecc51dbcfc40709504f
Gabriel Tan-Chen <gabriel@tan-chen.com>
2023-08-14 19:18:44 -0400

    GET /loans/:uuid

---
+++ app/routes/loans/item/get.js
@@ -0,0 +1,67 @@
+const { LoanError } = require('../../../middleware/error-handler');
+
+module.exports = (app) => {
+  const models = app.get('models');
+
+  /**
+   * @api {get} /loans/:uuid
+   * @apiName LoanItemGet
+   * @apiGroup Loan
+   *
+   * @apiSuccess (200) {object} data
+   * @apiSuccess (200) {object} data.attributes
+   * @apiSuccess (200) {integer} data.attributes.amount
+   * @apiSuccess (200) {integer} data.attributes.balance
+   * @apiSuccess (200) {string} data.attributes[created-at]
+   * @apiSuccess (200) {string} data.attributes.name
+   * @apiSuccess (200) {string} data.id
+   * @apiSuccess (200) {string} data.type
+   *
+   * @apiErrorExample {json} Error-Response:
+   *    HTTP/1.1 401 Unprocessable Entity
+   *    {
+   *      "errors": [{
+   *        "detail": "Unauthorized",
+   *      }],
+   *    }
+   */
+  return async(req, res, next) => {
+    try {
+      const user = await models.User.findOne({
+        attributes: ['household_uuid', 'uuid'],
+        where: {
+          uuid: req.userUuid,
+        },
+      });
+      if (!user) {
+        throw new Error('Unauthorized');
+      }
+
+      const loan = await models.Loan.findOne({
+        attributes: ['amount_cents', 'balance_cents', 'created_at', 'name', 'uuid'],
+        where: {
+          household_uuid: user.get('household_uuid'),
+          uuid: req.params.uuid,
+        },
+      });
+      if (!loan) {
+        throw new LoanError('Not found');
+      }
+
+      return res.status(200).json({
+        'data': {
+          'attributes': {
+            'amount': loan.get('amount_cents'),
+            'balance': loan.get('balance_cents'),
+            'created-at': loan.get('created_at'),
+            'name': loan.get('name'),
+          },
+          'id': loan.get('uuid'),
+          'type': 'loans',
+        },
+      });
+    } catch (err) {
+      return next(err);
+    }
+  };
+};
